4d049ee46070b950dbad7ed0f5b47a83d2cc4316
Wahid-Haidari <wahidullahhaidari2@gmail.com>
2023-04-01 12:13:18 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        
+        <h1>IT support</h1>
+        <p>
+         Get the IT support you need.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
