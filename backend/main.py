e982106940415bdd72e4e71d434458dfc1c992ea
JamshedK <karimnazarovj@gmail.com>
2023-04-01 23:37:16 -0500

    made the basic code ready for production

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from model import gpt_model
+from pydantic import BaseModel
 
+class Query(BaseModel):
+    query: str
+    
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def read_root(query: str):
+    return gpt_model.ask_the_model(query)
