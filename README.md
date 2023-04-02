07fe58817ffa950774efbd7cc4c66910b6813b74
JamshedK <karimnazarovj@gmail.com>
2023-04-02 11:51:47 -0500

    DONE

--- README.md
+++ README.md
@@ -1,14 +1,36 @@
-# Backend
+## Inspiration
+The whole team has been extremely excited with the introduction of ChatGPT and GPT-3 and 4 which can intereact with people in human-like conversations. Also, ChatGPT excels in many areas, it lacks very domain specific knowledge and we think that is an area we could improve. One example, working at University of Oklahoma IT support most of the requests we get are repeated and all we do is copy paste answers from a shared document we have. This is an area that GPT models could excel at. However, when we asked ChatGPT how I connect my XBox to AccessOU, here it gave an **incorrect** answer: 
+```
+AccessOU is an online learning platform provided by the University of Oklahoma, and it is not designed for connecting gaming consoles like Xbox. However, if you're looking to connect your Xbox to the internet through the university's network, you can follow these steps:
 
-# Frontend
+Connect your Xbox to the university's network using an Ethernet cable or a wireless connection. To connect wirelessly, go to the Xbox Dashboard, select "Settings," then "Network Settings," and choose the university's network.
 
-# Challenges and learnin outcomes
+If prompted, enter your university login credentials to authenticate your device and connect to the network.
 
+If you're having trouble connecting your Xbox to the network, contact the university's IT helpdesk for assistance.
+
+Note that while you can connect your Xbox to the university's network, you may not be able to play online games or access certain online features due to restrictions set by the university's network. Make sure to review the university's acceptable use policy for more information.
+
+```
+## What it does
+
+## How we built it
+
+## Challenges we ran into
 ### Jamshed
+#### Backend
+1. Keeping a server running in production in an AWS EC2 instance: We choose FastAPI (a python web framework) because it makes it very fast to set up a server that serves APIs. Fast API uses uvicorn to set up another server which serves the requests made to the API. I set up an nginx web server, which router to localhost port 8000 where the uvicorn server could respond to API requests. But the issue was that whenever I closed the terminal through which I was connected to, the uvicorn server went down. And the API wasn't accessible. I found out that I need to create systemd service file to keep the server running. All the steps that I took are outlined here.
+#### Frontend
+1. Conditional rendering: How to display a loading message and replace that with the actual response that comes from the backend. It took a lot of trial an error to finally make it work. I set up the gpt_reply prop to an empty text and in the component put a conditional rendering. If text is === '', then show the loading message, otherwise show the message that comes from GPT. I tried with isLoading state which gets set to false after the response comes from the backend, but that did not work.
+### Mohamed
+Data Collection and Preparation
+Web scraping for IT support articles: To collect data for our GPT-3 model, we built a web scraper to download articles from the IT support website. We used a list of URLs representing different categories on the website and built functions to download the HTML content, extract article links, and save the articles as text files. The BeautifulSoup library was used for parsing the HTML content, and we ensured the extracted content was cleaned by removing unnecessary elements such as images and replacing anchor tags with their text followed by the URL in parentheses. This process helped us gather a clean dataset for our model.
+Building the GPT-3 Model
+Creating a chatbot for IT support: We utilized OpenAI's GPT-3 model to create a chatbot that can answer IT support-related questions. The collected and cleaned dataset was used to fine-tune the GPT-3 model, improving its ability to provide accurate and contextually relevant responses for IT support queries. The chatbot was built using the llama_index library, which provides a convenient interface for indexing and querying the GPT-3 model.
 
-1. Backend
-- *Keeping a server running in production in an AWS EC2 instance:* We choose FastAPI (a python web framework) because it makes it very fast to set up a server that serves APIs. Fast API uses uvicorn to set up another server which serves the requests made to the API. I set up an nginx web server, which router to localhost port 8000 where the uvicorn server could respond to API requests. But the issue was that whenever I closed the terminal through which I was connected to, the uvicorn server went down. And the API wasn't accessible. I found out that I need to create systemd service file to keep the server running. All the steps that I took are outlined here. 
 
-2. Frontend
-- *Conditional rendering*: How to display a loading message and replace that with the actual response that comes from the backend. It took a lot of trial an error to finally make it work. I set up the gpt_reply prop to an empty text and in the component put a conditional rendering. If text is === '', then show the loading message, otherwise show the message that comes from GPT. I tried with isLoading state which gets set to false after the response comes from the backend, but that did not work. 
+## Accomplishments that we're proud of
 
+## What we learned
+
+## What's next for itsupport.ai
--- backend/model/get_it_articles.py
+++ backend/model/get_it_articles.py
@@ -1,72 +1,130 @@
-import string
-
-from bs4 import BeautifulSoup
-import requests
-import urllib.request
 import os
+import re
+import requests
+import string
+from bs4 import BeautifulSoup
 
+
+# Downloads the HTML content of a given URL and handles exceptions.
 def download_html(url):
     try:
-        r = requests.get(url)
-        r.raise_for_status()
-        return r.text
+        r = requests.get(url)  # Send an HTTP GET request to the given URL
+        r.raise_for_status()  # Raise an exception if the response contains an HTTP error status code
+        return r.text  # Return the HTML content of the response
     except requests.exceptions.RequestException as e:
-        print(f"Error downloading {url}: {e}")
+        print(f"Error downloading {url}: {e}")  # Print the error message
         return None
 
+
+# Parses the HTML content and returns a set of absolute article links.
 def articles_links_func(html, url):
-    soup = BeautifulSoup(html, 'html.parser')
-    links = set()
+    soup = BeautifulSoup(html, 'html.parser')  # Create a BeautifulSoup object to parse the HTML content
+    links = set()  # Initialize an empty set to store unique article links
+
+    # Iterate through all <a> elements with an href attribute
     for a in soup.find_all('a', href=True):
         link = a['href']
+
+        # If the link is relative, prepend the base URL to create an absolute link
         if not link.startswith('http'):
             link = url + link
-        links.add(link)
 
-    return links
+        links.add(link)  # Add the link to the set
 
+    return links  # Return the set of absolute article links
+
+
+# Cleans up a filename by removing invalid characters.
 def sanitize_filename(filename):
-    # Remove invalid characters from the filename
+    # Define a string of valid characters for a filename
     valid_characters = '-_.() %s%s' % (string.ascii_letters, string.digits)
+
+    # Filter out any invalid characters in the filename and return the cleaned-up string
     return ''.join(c for c in filename if c in valid_characters)
 
+
+# Downloads an article, extracts its content, and saves it as a text file.
 def save_article(url, output_dir):
     html = download_html(url)
     if not html:
         return
 
-    # Sanitize the filename to avoid issues with special characters
-    filename = sanitize_filename(url.split('/')[-1]) + '.html'
-    output_path = os.path.join(output_dir, filename)
+    soup = BeautifulSoup(html, 'html.parser')
 
-    with open(output_path, 'w', encoding='utf-8') as f:
-        f.write(html)
+    # Find the title element, extract its text or use a default text if not found
+    title = soup.find('h1', class_='gutter-top')
+    title_text = title.get_text() if title else 'No title found'
 
-    print(f"Saved {url} to {output_path}")
+    content_div = soup.find('div', id='divMainContent')
+    if content_div:
+        # Find all paragraph and list item elements within the content div
+        paragraphs_and_list_items = content_div.find_all(['p', 'li'])
+        content_text = ""
 
+        # Iterate through the elements and clean up their content
+        for element in paragraphs_and_list_items:
+            # Remove all <img> elements within the paragraph or list item
+            for img in element.find_all('img'):
+                img.decompose()
+
+            # Check if the element is not inside a container with class "btn btn-default"
+            if not element.find_parent(class_="btn btn-default"):
+                # Replace all <a> elements with their text followed by the URL in parentheses
+                for a in element.find_all('a', href=True):
+                    a.replace_with(f"{a.get_text()} ({a['href']})")
+
+                # Append the cleaned-up element text to the content_text variable
+                content_text += element.get_text() + "\n\n"
+    else:
+        content_text = "No content found"
+
+        # Combine the title and content text
+        extracted_text = f"{title_text}\n\n{content_text}"
+
+        # Replace non-breaking spaces with regular spaces
+        extracted_text = extracted_text.replace('\xa0', ' ')
+
+        # Sanitize the filename using the URL's last part and append the '.txt' extension
+        filename = sanitize_filename(url.split('/')[-1]) + '.txt'
+        output_path = os.path.join(output_dir, filename)
+
+        # Write the extracted text to a file in the specified output directory
+        with open(output_path, 'w', encoding='utf-8') as f:
+            f.write(extracted_text)
+
+        print(f"Saved {url} to {output_path}")
+
+# Iterates through a list of URLs and downloads articles for each category.
 def download_articles(urls, output_dir):
+    # Define a regex pattern to match the desired article URLs
+    article_pattern = re.compile(r'https://itsupport\.ou\.edu/TDClient/\d+/Norman/KB')
 
+    # Iterate through the provided list of URLs
     for url in urls:
-        # Extract the base URL to handle relative links
+        # Extract the base URL to be used for constructing absolute links from relative links
         base_url = '/'.join(url.split('/')[:3])
 
-        # Download the HTML content of the main URL
+        # Download the HTML content of the current URL
         html = download_html(url)
         if not html:
-            continue
+            continue  # If the download failed, skip to the next URL
 
-        # Get the article links from the downloaded HTML content
+        # Extract article links from the downloaded HTML content
         article_links = articles_links_func(html, base_url)
 
-        # Download and save each article to the output directory
+        # Iterate through the extracted article links
         for link in article_links:
-            save_article(link, output_dir)
+            # Check if the link matches the desired article URL pattern
+            if article_pattern.match(link):
+                # Save the article's content as a text file in the specified output directory
+                save_article(link, output_dir)
 
-
-urls = ["https://itsupport.ou.edu/TDClient/35/Norman/KB/?CategoryID=13", "https://itsupport.ou.edu/TDClient/35/Norman/KB/?CategoryID=14"
+# Define a list of URLs representing different categories on the IT support website
+urls = ["https://itsupport.ou.edu/TDClient/35/Norman/KB/?CategoryID=13", "https://itsupport.ou.edu/TDClient/35/Norman/KB/?CategoryID=14",
         "https://itsupport.ou.edu/TDClient/35/Norman/KB/?CategoryID=15", "https://itsupport.ou.edu/TDClient/35/Norman/KB/?CategoryID=16",
         "https://itsupport.ou.edu/TDClient/35/Norman/KB/?CategoryID=17", "https://itsupport.ou.edu/TDClient/35/Norman/KB/?CategoryID=18",
         "https://itsupport.ou.edu/TDClient/35/Norman/KB/?CategoryID=19", "https://itsupport.ou.edu/TDClient/35/Norman/KB/?CategoryID=20",
         "https://itsupport.ou.edu/TDClient/35/Norman/KB/?CategoryID=21"]
 
-download_articles(urls, 'articles')
+# Call the download_articles function with the list of URLs and the output directory 'articles_txt'
+download_articles(urls, 'articles_txt')
